1ca932ffd094027e1cce244419f49ed6f6b8d3ee
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2025-03-17 19:25:14 -0300

    terminando o bubble sort

--- aula4/bubble_sort.py
+++ aula4/bubble_sort.py
@@ -1,10 +1,12 @@
-lista = [23, 12, 6, 34, 12, 9, 10, 1, 5]
+def bubble_sort(lista):
+    x = 0
+    for vezes in range(len(lista)-1, 0, -1):
+        for i in range(vezes):
+            if lista[i] > lista[i+1]:
+                x = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = x
+    return lista
 
-for i in range(len(lista)):
-    maior = lista[0]
-    menor = 0
-    if lista[i] < maior:
-        menor = lista[i]
-        lista[i] = menor
-        lista[i+1] = maior
-    print(lista)
+
+print(bubble_sort([23, 12, 6, 34, 12, 9, 10, 1, 5]))
